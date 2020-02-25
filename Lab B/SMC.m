f88af26d475a7a568d68c038dee55f1eeeb7d6f2
Martin Blaszczyk <marbla-6@student.ltu.se>
2020-02-25 15:43:49 +0100

    SMC done

---
+++ Lab B/SMC.m
@@ -0,0 +1,5 @@
+% Sliding Mode Controller
+p = [-0.2, -0.2];
+x = [10, 10]
+sigma = p(1)*x(1) + p(2)*x(2);
+
---
+++ Lab B/kalman_dynamic.m
@@ -0,0 +1,47 @@
+
+A = 33;
+a = 0.16;
+T = 35;
+K = 5;
+g = 981;
+
+h10 = 8;
+h20 = 7;
+u0 = 1.3;
+
+x0 = [h10; h20];
+P0 = 1*eye(2);
+
+
+Amat = [ -(a*sqrt(2*g))/(2*A*sqrt(h10)) 0 ;
+                 (a*sqrt(2*g))/(2*A*sqrt(h10)) -(a*sqrt(2*g))/(2*A*sqrt(h20))];
+Bmat = [6.22/A; 0];
+Cmat = [0 1];
+Dmat = zeros(1);
+
+
+Ts = 1e-1;
+SYS = ss(Amat,Bmat,Cmat,Dmat);
+DSYS = c2d(SYS,Ts);
+
+Amatd = DSYS.A;
+Bmatd = DSYS.B;
+Cmatd = DSYS.C;
+
+
+
+% P = lyap(Amat, Q); Förmodligen onödig
+
+
+% [P,K,L] = idare(DSYS.A,DSYS.B,Q)  % X = P,
+
+
+
+2
+
+
+% blockschemat ska baseras på följande för kalmanfilteret
+% x_hat_prick = Ax_hat + Bu + K(y - y_hat)
+% y = Cx_hat + Du
+
+
