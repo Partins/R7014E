ee53527b27e9201eb67441866f97849c451c41f1
Partins <marblapas@gmail.com>
2020-03-29 12:50:21 +0200

    working Kalman

--- Lab C/initialize_lab_c_kalman.m
+++ Lab C/initialize_lab_c_kalman.m
@@ -14,27 +14,36 @@ B = [	0		, 0		, 	0		;
 C =  [  1, 0, 0, 0, 0;
         0, 1, 0, 0, 0;
         0, 0, 1, 0, 0];
-    
+
 D = zeros(3);
 
 s = tf('s');
-tf_sys = (C*inv(s*eye(5)-A))*B + D;
-ss_sys = ss(A, B, C, D);
+%tf_sys = (C*inv(s*eye(5)-A))*B + D;
+%ss_sys = ss(A, B, C, D);
 
-if rank(ctrb(ss_sys)) ~= 5
-    disp('Not Controllable')
-end
-if rank(obsv(ss_sys)) ~= 5
-    disp('Not Observable')
-end
+% if rank(ctrb(ss_sys)) ~= 5
+%     disp('Not Controllable')
+% end
+% if rank(obsv(ss_sys)) ~= 5
+%     disp('Not Observable')
+% end
 
 %% Kalman
-Vd = .1*eye(size(A));
+Vd = 0.1*eye(size(A));
 Vn = 1*eye(3);
 [Kf, P, E] = lqe(A, Vd, C, Vd, Vn);
 
+%% LQR
+Q = [1 0 0 0 0;
+     0 100 0 0 0;
+     0 0 10 0 0;
+     0 0 0 1 0;
+     0 0 0 0 1]
 
+R = 0.1;
+L = lqr(A-Kf*C,B,Q,R)
 %[P,K,L] = icare(A,B,Q,R)  % X = P,
+
 
 
 
